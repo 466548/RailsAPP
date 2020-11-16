dcecf300a3f40e4eb0da38b578cc7cc48e0354b8
466548 <tsuka.as@gmail.com>
2020-11-16 23:56:43 +0900

    htmlか

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,13 @@
 class HelloController < ApplicationController
   def index
-    render plain:"hello, Tzzzhis Rails sample"
+    msg = '
+    <html>
+      <body>
+        <h1>眠たい</h1>
+        <p>眠たい眠たい眠たい眠たい眠たい眠たい眠たい眠たい眠たい</p>
+      </body>
+    </html>
+    '
+    render html: msg.html_safe
   end
 end
