2ba604e450d94af104dbe12cd5a2b891746ca943
466548 <tsuka.as@gmail.com>
2020-11-28 09:32:55 +0900

    css 追加

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,4 @@
 class HelloController < ApplicationController
   def index
-    msg = '
-    <html>
-      <body>
-        <h1>眠たい</h1>
-        <p>眠たい眠たい眠たい眠たい眠たい眠たい眠たい眠たい眠たい</p>
-      </body>
-    </html>
-    '
-    render html: msg.html_safe
   end
 end
